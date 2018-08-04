f55358255ef619a0976fbee00af780991bb2597e
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-08-05 00:43:09 +0800

    Update mgtUI.php

--- src/petui/mgtUI.php
+++ src/petui/mgtUI.php
@@ -12,80 +12,42 @@ class petUI
 	public function __construct(CI $pg) {
         	$this->main = $pg;
     	}
-
-    	public function customForm(Player $player, $wooosh)
-    	{
-		$form = $this->main->formapi->createCustomForm(function (Player $player, array $data) {
-		    if( isset($data[1]) )
-		    {
-			$pref = $data[1]; //pet name
-			$nn = $data[2]; //baby
-			$poor = $data[3]; //target name
-
-			if(strlen($pref) <= 0 or strlen($pref) >= 9 ) {
-				$player->sendMessage('<•> Error! 1 - 8 characters are allowed');
-				return true;
-			}
-
-			$baby = 'false';
-			if($nn) {
-				$baby = 'true';
-			}
-
-			$target = null;
-			if($poor !== "") {
-				$target = $poor;
-			}
-
-				$this->main->applyPetRequest($player, $pref, (float) 1.0, $baby, $target);
-		    }
-			return true;
-		});
-
-		$form->setTitle('§l§cPet§f Config');
-		//data[0]
-		$skadoossh = $this->main->getPrice($wooosh);
-		$form->addLabel("§fPet type: $wooosh (§a $skadoossh §f)§r\n§cNote: some pets need you to relogin to be turned into baby, and some has no baby form.");
-		//$data[1]
-		$form->addInput("§oPetName §8(max of 8 chars.)");
-		//$data[2]
-		$form->addToggle("§oBaby Form?", false);
-		//$data[3]
-		$form->addInput("§oGift to a player (optional | auto-complete)");
-		$form->sendToPlayer($player);
-    	}
 	
-	public function normalForm(Player $player, $wooosh)
+	public function sendMainMenu(Player $player)
     	{
-		$form = $this->main->formapi->createCustomForm(function (Player $player, array $data) {
-		    if( isset($data[1]) ) {
+		$form = $this->formapi->createSimpleForm(function (Player $player, array $data) {
+		    if (isset($data[0])){
+			switch ($data[0])
+			{
+				case 0:
+					$this->sendNormalMenu($player);
+				break;
 
-			$pref = $data[1]; //pet name
-			$poor = $data[2]; //target name
+				case 1:
+					$player->sendMessage("Feature is in progress");//$this->sendExoMenu($player);
+				break;
 
-			if(strlen($pref) <= 0 or strlen($pref) >= 9 ) {
-			    $player->sendMessage('<•> Error! Only 1 - 8 characters are allowed');
-			    return true;
+				case 2:
+					if($player->hasPermission("shs.vip.pet"))
+					{
+						$this->sendVIPMenu($player);
+					} else {
+						$player->sendMessage("§cPlease visit the store and purchase VIP");
+					}
+				break;
+				default:
+					$this->mgt->sendUI($player);			
 			}
-
-			$target = null;
-			if($poor !== "") {
-			    $target = $poor;
-			}
-
-			$this->main->applyPetRequest($player, $pref, (float) 1.0, "false", $target);
+			return true;
 		    }
-			return true;	
 		});
-        
-		$form->setTitle('§l§cPet§f Config');
-		//data[0]
-		$skadoossh = $this->main->getPrice($wooosh);
-		$form->addLabel("§fPet type: $wooosh (§a $skadoossh §f)");
-		//$data[1]
-		$form->addInput("§oPetName §8(max of 8 chars.)§c*");
-		//$data[2]
-		$form->addInput("§oGift to a player (optional | auto-complete)");
+		$form->setTitle('§l§fPet Store');
+
+		$form->addButton('§l§0Toggle All Pets'); //data[0]
+		$form->addButton('§l§0Release A Pet'); //data[1]
+		$form->addButton('§l§0Change-name'); //data[2]
+		$form->addButton('§l§0Top Pets'); //data[2]
+
 		$form->sendToPlayer($player);
-    	}
+	}
 }
