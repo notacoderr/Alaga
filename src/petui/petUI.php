21d07408ed3fe564cdea45c62a453494b00a4ebf
notacoderr <38270548+notacoderr@users.noreply.github.com>
2018-08-04 23:58:52 +0800

    Update petUI.php

--- src/petui/petUI.php
+++ src/petui/petUI.php
@@ -10,52 +10,80 @@ class petUI
     public $main;
 	
 	public function __construct(CI $pg) {
-        $this->main = $pg;
-    }
+        	$this->main = $pg;
+    	}
 
-    public function mainForm(Player $player, $wooosh, $skadoossh)
-    {
-        $form = $this->main->formapi->createCustomForm(function (Player $player, array $data) {
-            $price = 0;
-            if( isset($data[1]) ) {
-				$pref = $data[1]; //pet name
-                $nn = $data[2]; //baby
-                $poor = $data[3]; //target name
+    	public function customForm(Player $player, $wooosh, $skadoossh)
+    	{
+		$form = $this->main->formapi->createCustomForm(function (Player $player, array $data) {
+		    if( isset($data[1]) )
+		    {
+			$pref = $data[1]; //pet name
+			$nn = $data[2]; //baby
+			$poor = $data[3]; //target name
 
-				if(strlen($pref) <= 0 or strlen($pref) >= 9 ) {
-                    $player->sendMessage('<•> Error! 1 - 8 characters are allowed');
-                    return true;
-				}
+			if(strlen($pref) <= 0 or strlen($pref) >= 9 ) {
+				$player->sendMessage('<•> Error! 1 - 8 characters are allowed');
+				return true;
+			}
 
-                $baby = 'false';
-                if($nn) {
-                    $price = $price + $this->main->settings->getNested('price.others.baby');
-                    $baby = 'true';
-                }
+			$baby = 'false';
+			if($nn) {
+				$baby = 'true';
+			}
 
-                $target = null;
-                if($poor !== "") {
-                    $target = $poor;
-                }
+			$target = null;
+			if($poor !== "") {
+				$target = $poor;
+			}
 
-                $this->main->applyPetRequest($player, $pref, (float) 1.0, $baby, $price, $target);
-            }
+				$this->main->applyPetRequest($player, $pref, (float) 1.0, $baby, $target);
+		    }
 			return true;
-			
-        });
+		});
+
+		$form->setTitle('§l§cPet§f Config');
+		//data[0]
+		$form->addLabel("§fPet type: $wooosh (§a $skadoossh §f)§r\n§cNote: some pets need you to relogin to be turned into baby, and some has no baby form.");
+		//$data[1]
+		$form->addInput("§oPetName §8(max of 8 chars.)");
+		//$data[2]
+		$form->addToggle("§oBaby Form?", false);
+		//$data[3]
+		$form->addInput("§oGift to a player (optional | auto-complete)");
+		$form->sendToPlayer($player);
+    	}
+	
+	public function normalForm(Player $player, $wooosh, $skadoossh)
+    	{
+		$form = $this->main->formapi->createCustomForm(function (Player $player, array $data) {
+		    if( isset($data[1]) ) {
+
+			$pref = $data[1]; //pet name
+			$poor = $data[2]; //target name
+
+			if(strlen($pref) <= 0 or strlen($pref) >= 9 ) {
+			    $player->sendMessage('<•> Error! Only 1 - 8 characters are allowed');
+			    return true;
+			}
+
+			$target = null;
+			if($poor !== "") {
+			    $target = $poor;
+			}
+
+			$this->main->applyPetRequest($player, $pref, (float) 1.0, "false", $target);
+		    }
+			return true;	
+		});
         
-        $form->setTitle('§l§dKawaii §fPet Store');
-        //data[0]
-        $form->addLabel("§fPet type: $wooosh (§a $skadoossh §f)§r\n§fFields with §c*§f are required.\n§eType the exact name of the player, dont replace spaces or add special characters\n§cNote: some pets need u to relogin to be turned into baby, and some just stay as adult (no baby form).");
-
-        //$data[1]
+		$form->setTitle('§l§cPet§f Config');
+		//data[0]
+		$form->addLabel("§fPet type: $wooosh (§a $skadoossh §f)");
+		//$data[1]
 		$form->addInput("§oPetName §8(max of 8 chars.)§c*");
-
-        //$data[2]
-        $form->addToggle("§oBaby Pet? add:§a $" . $this->main->settings->getNested('price.others.baby'), false);
-
-        //$data[3]
-        $form->addInput("§oGift to a player (optional | auto-complete)");
-        $form->sendToPlayer($player);
-    }
+		//$data[2]
+		$form->addInput("§oGift to a player (optional | auto-complete)");
+		$form->sendToPlayer($player);
+    	}
 }
